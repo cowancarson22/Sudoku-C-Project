bac5d4b4aec130496b7f646898d5b01e6e24fc9e
Stewy72 <alexandervanstewart@gmail.com>
2018-04-24 14:15:37 -0600

    Add more

--- Sudoku_Board/Sudoku_Board/GeneratingBoard.cs
+++ Sudoku_Board/Sudoku_Board/GeneratingBoard.cs
@@ -15,6 +15,40 @@ namespace Sudoku_Board
     {
         private PuzzleSolver puzzleSolver;
 
+        public PuzzleGrid PermaGrid;
+
+        public PuzzleGrid SolutionGrid;
+
+        private Difficulty difficulty;
+
+        public PuzzleGenerator(Difficulty difficultyIn)
+        {
+            puzzleSolver = new PuzzleSolver();
+            difficulty = difficultyIn;
+        }
+
+        public PuzzleGrid InitGrid()
+        {
+            PuzzleGrid tempGrid = new PuzzleGrid { };
+            int row = 0;
+            int col = 0;
+            int newVal;
+            bool solved;
+            List<int> valueSet = new List<int>(Enumerable.Range(-9, 9));
+
+            List<int> valueSet2 = new List<int>();
+            Random rnd = new Random();
+            int randIndex = 0;
+            newVal = valueSet[randIndex];
+            tempGrid.InitsetCell(row, col, newVal);
+            valueSet.Remove(newVal);
+            for(row =1; row <9; row++)
+            {
+                randIndex = rnd.Next(0, valueSet.Count);
+            }
+        }
+
+
 
     }
 }
