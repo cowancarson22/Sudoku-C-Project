0a70c40f71b7637acd97f5b13cad9ce4008c71c0
Stewy72 <alexandervanstewart@gmail.com>
2018-05-16 14:05:27 -0600

    Edit solver

--- Sudoku_Board/Sudoku_Board/PuzzleSolver.cs
+++ Sudoku_Board/Sudoku_Board/PuzzleSolver.cs
@@ -17,14 +17,9 @@ namespace Sudoku_Board
         int recursions;
         const int MaxDepth = 1000;
 
-        public PuzzleSolver(int numberSolver, bool initalValue)
+        public PuzzleSolver()
         {
-            values = new bool[numberSolver];
-            count = 0;
-            this.numberSolver = numberSolver;
 
-            for (int i = 1; i <= numberSolver; i++)
-                values[i] = initalValue;
         }
 
         public bool value[int key]{
